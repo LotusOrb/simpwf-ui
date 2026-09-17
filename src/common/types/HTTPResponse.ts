fb63f565e7f863c54b3d6715b638aa1aa0c1e223
lotusorb <working.rsyafridamara@gmail.com>
2026-09-18 01:44:09 +0700

    feat: split typing

---
+++ src/common/types/HTTPResponse.ts
@@ -0,0 +1,9 @@
+import type { ComplexQueryParam } from './ComplexQueryParam';
+
+export type HTTPResponse<T> = {
+  code: number;
+  message: string;
+  explain: string;
+  data: T;
+  param: ComplexQueryParam;
+};
